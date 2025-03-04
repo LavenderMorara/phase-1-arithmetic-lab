03cfe50ca993f20411026dddc0353f5b3542f8e9
LavenderMorara <chiyamorara@gmail.com>
2025-03-04 11:51:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=62;
+const num2=1;
+const multiply=num1*num2;
+console.log(multiply)
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1; 
+
+const num3=10;
+const num4=6;
+const mod=num3%num4;
+
+const numbers=[1,7,3,7,14,17,20];
+const max=Math.max(...numbers);
